1d5efd5e9d84afef4ddf32910c1cd80a62b01d4f
RemyC <remycottrez@gmail.com>
2022-05-13 12:24:51 +0200

    contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,9 +3,42 @@ import Nav from "../components/Nav";
 
 const Contact = () => {
     return (
+        <>
+        <Nav />
         <div className='contact'>
-            <Nav />
+            <title>Contactez-| ShoeLaLa</title>
+            <header>
+                <h1>Contact</h1>
+            </header>
+            <hr></hr>
+            <section>
+                <ul>
+                    <li>
+                        <h2>Service client</h2>
+                        <adress>
+                            <a href="mailto: service.client@lalashoes.com">Envoyer un mail</a>
+                            <a>0 800 800 800</a>
+                        </adress>
+                    </li>
+                    <li>
+                        <h2>Assistance LaLaShoes</h2>
+                        <adress>
+                            <a href="mailto: assistance@lalashoes.com">Envoyer un e-mail</a>
+                            <a>0 700 700 700</a>
+                        </adress>
+                    </li>
+                    <li>
+                        <h2>Service client</h2>
+                        <a href="mailto: service.client@lalashoes.com">Envoyer un e-mail</a>
+                    </li>
+                    <li>
+                        <h2>Service client</h2>
+                        <a href="mailto: service.client@lalashoes.com">Envoyer un e-mail</a>
+                    </li>
+                </ul>
+            </section>
         </div>
+        </>
     );
 };
 
