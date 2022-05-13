5ad11d26e6f999374b4429445c5bb5fccf366fbf
romain <r.monier0@laposte.net*>
2022-05-13 14:37:39 +0200

    magasin

--- src/pages/Magasins.js
+++ src/pages/Magasins.js
@@ -3,9 +3,31 @@ import Nav from '../components/Nav'
 
 const Magasins = () => {
     return (
-        <div className='magasins'>
+        <>
             <Nav />
+        <div className='magasins'>
+            <h1>BOUTIQUES</h1>
+            <br></br>
+            <ul>
+<li><h3>Boutique chic</h3>
+    <p>17 Rue René Lanoy,62300 Lens</p>
+    </li>
+<li><h3>Boutique top</h3>
+<p>177 rue Nationale,59000 Lille</p>
+</li>
+<li><h3>Chauss 2 Luxe</h3>
+<p>35 Boulevard Haussmann,75008 Paris</p>
+</li>  
+<li><h3>Chauss to go</h3>
+<p>12 rue au Ours,62000 Arras</p>
+</li>  
+<li><h3>Chausse sure</h3>
+<p>12 Avenue du Maréchal Foch 13004 Marseille</p>
+</li>
+
+           </ul>
         </div>
+        </>
     );
 };
 
