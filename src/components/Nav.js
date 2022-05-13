ba8dc739c2ce0ad82f485c2abab39add1520197a
evilKhory <felldren.montage@gmail.com>
2022-05-13 10:08:11 +0200

    Nav placeholder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <div className='navigation'>
-            
+            <ul>
+                <NavLink to="/">
+                    <li>Accueil</li>
+                </NavLink>
+                <NavLink to="/Boutique">
+                    <li>Boutique</li>
+                </NavLink>
+                <NavLink to="/Magasins">
+                    <li>Magasins</li>
+                </NavLink>
+                <NavLink to="/Contact">
+                    <li>Contact</li>
+                </NavLink>
+            </ul>
         </div>
     );
 };
