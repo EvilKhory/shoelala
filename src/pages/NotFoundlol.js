63c16a7c23d2262c3b5020e5a304a3d15b79b25b
evilKhory <felldren.montage@gmail.com>
2022-05-13 12:22:48 +0200

    add

--- src/pages/NotFoundlol.js
+++ src/pages/NotFoundlol.js
@@ -4,13 +4,16 @@ import Franklin from '../assets/img/perdu.jpg'
 
 const NotFoundlol = () => {
     return (
+    <>
+    <Nav />
         <div className='notFoundLol'>
-            <Nav />
+            
             <h1>404 Franklin not found</h1>
             <div className='perdu'>
             <img src={Franklin} alt="désolé j'étais obligé d'y placer franklin avec des cubes marqué, gros taré"/>
             </div>
         </div>
+    </>
     );
 };
 
