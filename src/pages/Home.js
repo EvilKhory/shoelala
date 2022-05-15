2ce380829e92a31a3b1ec02d77442d8f7b9ad272
RemyCTRZ <remycottrez@gmail.com>
2022-05-15 14:13:32 +0200

    page d'accueil finie (reste footer à rajouter)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,68 +1,91 @@
 import React from "react";
 import Nav from "../components/Nav";
-import home1 from "../assets/img/home1.png";
+import home1 from "../assets/video/video.mp4";
 import home2 from "../assets/img/home2.png";
 import home3 from "../assets/img/home3.png";
 import home4 from "../assets/img/home4.png";
+import bvlgary from "../assets/img/Bvlgari.png";
+import cartier from "../assets/img/Cartier.png";
+import chanel from "../assets/img/Chanel.png";
+import louisvuitton from "../assets/img/Louis_Vuitton.png";
+import logobase from "../assets/img/shoelala.png";
 
 const Home = () => {
   return (
     <>
       <Nav />
       <div className="home">
-        <img src={home1} />
-        <article className="presentation">
-          <h1>Depuis 1924,</h1>
-          <hr></hr>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio,
-            suscipit ducimus temporibus repellat nemo rem distinctio quaerat
-            vero culpa nesciunt nihil, deleniti quas, ullam autem modi numquam
-            et? Minus, rem sequi officia nam alias beatae? Adipisci consectetur
-            sapiente itaque soluta tempore quaerat doloribus voluptas quod
-            recusandae nesciunt eaque beatae officiis libero ratione illum
-            temporibus, ipsa at facilis error quam totam deleniti. Labore eos
-            nulla eum excepturi similique fuga, exercitationem sunt, saepe
-            quaerat pariatur expedita? Officia beatae praesentium molestiae
-            consequatur voluptates porro ipsum rerum hic nemo placeat facilis
-            esse ut vero nobis fuga, quia libero tenetur molestias illo nesciunt
-            commodi et dolor franklin id pariatur? Repellendus, beatae.
-            Perferendis dicta quas fuga sapiente, sit minima, franklin veniam
-            doloremque soluta dolore odit unde modi rem magni quis adipisci ut,
-            doloribus accusantium corrupti. Pariatur, aliquam, facilis minima
-            animi quia eaque perferendis, totam error cumque temporibus
-            doloremque. Odio explicabo ducimus aspernatur culpa earum, nam quo
-            praesentium numquam quas optio nesciunt facere temporibus dolores,
-            eius illo molestiae eaque provident exercitationem tenetur
-            reprehenderit. Obcaecati libero voluptate officiis recusandae
-            incidunt dolores, nisi pariatur franklin sed doloremque tempora
-            quibusdam hic consectetur illum ex in iusto! Numquam quas eligendi,
-            franklin reiciendis ipsam quisquam veritatis? Magni distinctio
-            atque, laudantium quia labore incidunt voluptatem omnis nemo neque,
-            nulla, nesciunt fugiat consequuntur. Dolorum quidem totam, optio
-            aspernatur quia illo nisi officia adipisci natus minima sapiente
-            iusto. Sapiente laboriosam esse, nemo doloribus ab natus
-            perspiciatis ex eius quibusdam magni, suscipit delectus animi
-            aspernatur, placeat optio beatae. Minima vitae error reiciendis
-            doloremque porro reprehenderit illum eveniet facilis non blanditiis?
-          </p>
+        <video src={home1} disablePictureInPicture preload="auto" loop autoPlay muted/>
+        <section className="presentation">
+          <article className="depuis">
+            <h1>Depuis 1924</h1>
+            <hr></hr>
+            <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </p>
+            <img src={logobase} alt="logo shoelala"/>
+          </article>
             
-          <div className="sectionHomme">
+          <div className="btn_collection">
             <h1>COLLECTION POUR HOMMES</h1>
             <button className="btnHome"> Découvrir </button>
-            <img src={home2} />
+            <img src={home2} alt="section homme"/>
           </div>
-          <div className="sectionFemme">
+
+          <article className="partenaires">
+            <h1>Nos partenaires</h1>
+            <ul>
+              <li>
+                  <img src={bvlgary} alt="bvlgary"/>
+              </li>
+              <li>
+                  <img src={cartier} alt="cartier"/>
+              </li>
+              <li>
+                  <img src={chanel} alt="chanel"/>
+              </li>
+              <li>
+                  <img src={louisvuitton} alt="louis vuitton"/>
+              </li>
+            </ul>
+          </article>
+
+          <div className="btn_collection">
             <h1>COLLECTION POUR FEMMES</h1>
             <button className="btnHome"> Découvrir </button>
-            <img src={home3} />
+            <img src={home3} alt="section femme"/>
           </div>
-          <div className="sectionEnfant">
+
+          <article className="avis">
+            <div>
+              <h1>Les avis de nos clients</h1>
+              <hr></hr>
+            </div>
+            <ul>
+              <li>
+                <p><strong>Clément Berni</strong></p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
+                <p><span>08/05/2022 | 12:32</span></p>
+              </li>
+              <li>
+                <p><strong>Alexandre Leroi</strong></p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."</p>
+                <p><span>03/04/2022 | 17:42</span></p>
+              </li>
+              <li>
+                <p><strong>Zoe Decroix</strong></p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
+                <p><span>13/05/2022 | 07:58</span></p>
+              </li>
+            </ul>
+          </article>
+
+          <div className="btn_collection">
             <h1>COLLECTION POUR ENFANTS</h1>
             <button className="btnHome"> Découvrir </button>
-            <img src={home4} />
+            <img src={home4} alt="section enfant"/>
           </div>
-        </article>
+        </section>
       </div>
     </>
   );
