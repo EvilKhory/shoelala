8cd4e81840a03adb5a13809c20869b994bc06f25
RemyCTRZ <remycottrez@gmail.com>
2022-05-15 15:01:14 +0200

    fini (ajouter le footer toujours)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -74,7 +74,7 @@ const Home = () => {
               </li>
               <li>
                 <p><strong>Zoe Decroix</strong></p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."</p>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
                 <p><span>13/05/2022 | 07:58</span></p>
               </li>
             </ul>
