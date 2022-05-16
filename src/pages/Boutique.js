6c7f066cb748d8c73c9d2887724712a669c2c628
kyllianbou <kyllian.boussaid@gmail.com>
2022-05-16 11:10:06 +0200

    krkr

--- src/pages/Boutique.js
+++ src/pages/Boutique.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Tab from './tableau';
 import Nav from "../components/Nav";
 
 const Boutique = () => {
     return (
         <div className='Boutique'>
             <Nav />
+        <Tab/>
         </div>
     );
 };
