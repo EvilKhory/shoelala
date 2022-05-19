2d4ef9801179d76562be771d97f7f4cd5133c41c
evilKhory <felldren.montage@gmail.com>
2022-05-19 16:08:37 +0200

    redirection

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <BrowserRouter> 
       <Routes>
+      <Route path='/' element={<Home />} />
       <Route path='/shoelala' element={<Home />} />
       <Route path='/Contact' element={<Contact />} />
       <Route path='/Magasins' element={<Magasins />} />
