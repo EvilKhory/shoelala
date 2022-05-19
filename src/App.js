9aa0f0e5d1e32860c7f6274d7a034f4f32f1cbd8
RemyC <remycottrez@gmail.com>
2022-05-19 12:13:05 +0200

    react importé

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './pages/Contact';
 import Boutique from './pages/Boutique';
 import './styles/index.scss';
 import NotFoundlol from './pages/NotFoundlol';
+import React from 'react';
 
 function App() {
   return (
